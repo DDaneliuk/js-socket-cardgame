bcdf33b456823aaece1888e7af192cbe16b313a2
DDenys <d.danelyuk@gmail.com>
2021-07-05 22:35:52 +0300

    Update

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const signupRoutes = require('./routes/signup')
 const loginRoutes = require('./routes/login')
 
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server); // docs: https://socket.io/docs/v3/server-initialization/
 
 const hbs = exphbs.create({
     defaultLayout: "main",
@@ -24,9 +26,29 @@ app.use('/', homeRoutes)
 app.use('/signup', signupRoutes)
 app.use('/login', loginRoutes)
 
+const socketRooms = new Map();
+
+io.on('connection', async socket => {
+  const sid = socket.id;
+
+  console.log("socket connected: ", sid);
+
+  socket.emit("huy", "ti loh!")
+
+  socket.on("d1", msg => {
+    console.log("d1 event, data: ", msg);
+  })
+
+  socket.on("d2", msg => {
+    console.log("d2 event, data: ", msg);
+  })
+
+  socket.on("disconnect", () => {
+    console.log("socket disconnected, id: ", socket.id);
+  })
+})
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 })
